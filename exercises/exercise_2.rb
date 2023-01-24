dc460abadd3058041c66da4537d1c90bf0b5ce9e
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-01-23 19:12:56 -0800

    AR queries CRUD stores database

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Change the name of two stores
+
+@store1 = Store.find_by(id: 1)
+@store1.update(name: "Burnaby Clothes")
+puts @store1.inspect
+
+@store2 = Store.find_by(id: 2)
+@store2.update(name: "Richmond Clothes")
+puts @store2.inspect
+
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,32 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create 3 more stores
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+#puts surrey.inspect
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+#puts whistler.inspect
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+#puts yaletown.inspect
+
+puts Store.count #5
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "Mens stockists and revenue"
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+#puts @mens_stores.inspect
+
+@women_underperforming = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+puts "Womens stockists under $1m in revenue"
+@women_underperforming.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+#puts @women_underperforming.inspect
